a27561ec093e27513c762c98842a079ad332eb3a
Darek <gawlikdaarek@gmail.com>
2023-02-12 10:17:48 +0100

    docs: inital commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe-list/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'a test recipe',
+      'this is simply a test',
+      'https://realfood.tesco.com/media/images/1400x919-Arrabbiata-meatball-bake-aff103a1-f93b-4646-913b-8f5b01d213e3-0-1400x919.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simply a test',
+      'https://realfood.tesco.com/media/images/1400x919-Arrabbiata-meatball-bake-aff103a1-f93b-4646-913b-8f5b01d213e3-0-1400x919.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
